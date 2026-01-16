aaad63091f0740b52d973c966102f8b099f03568
Gabriel-Perez-Marquez <gabrielperezmarquez18@gmail.com>
2026-01-16 09:07:30 +0100

    Proyecto inicializado

---
+++ src/main/java/com/salesianostriana/dam/EventifyLite/EventifyLiteApplication.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.EventifyLite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventifyLiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventifyLiteApplication.class, args);
+	}
+
+}
